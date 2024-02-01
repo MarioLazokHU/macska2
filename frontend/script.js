ee8447e29fef265c3f73d3ed9d01bf3ef2c9d735
MarioLazokHU <lazokmarios7@gmail.com>
2024-02-01 17:49:26 +0100

    új feladat

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,1 @@
-const API_KEY = "59IQA9hsqzi09UnfMkxfHkkbKEA8OQCMg0QxgsNs";
-//`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=2023-05-${day}`
+'Nasa API' = `https://api.nasa.gov/planetary/apod?api_key=59IQA9hsqzi09UnfMkxfHkkbKEA8OQCMg0QxgsNs&date=${date.value}`
